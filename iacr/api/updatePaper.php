9eac6a59ebaf1f2b1853d2cfa4b5dd43ca270f3b
Kevin McCurley <github@digicrime.com>
2026-02-12 00:31:23 +0000

    Fix callback api.

--- iacr/api/updatePaper.php
+++ iacr/api/updatePaper.php
@@ -3,7 +3,7 @@ require 'lib.php';
 // This allows the submit server to update a paper to record
 // the final version was uploaded, or the slides were uploaded,
 // or a video was uploaded.
-global $Opt, $Conf;
+global $Opt;
 
 header('Content-Type: application/json');
 if (empty($_POST['paperId'])) {
@@ -37,9 +37,19 @@ if (empty($_POST['action']) || $_POST['action'] !== 'finalPaper') {
   exit;
 }
 try {
-    $Conf->q("UPDATE Paper set timeFinalSubmitted=? WHERE paperId=?", $Conf::$now, $_POST['paperId']);
-    $Conf->q("DELETE FROM PaperOption WHERE paperId=? AND optionId=?", $_POST['paperId'], PaperOption::IACR_FINAL_ID);
-    $Conf->q("INSERT INTO PaperOption (paperId,optionId,value) VALUES (?,?,1)", $_POST['paperId'], PaperOption::IACR_FINAL_ID);
+    $dsn = 'mysql:host=localhost;dbname=' . $Opt['dbName'];
+    $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+    $db = new PDO($dsn, $Opt['dbUser'], $Opt['dbPassword'], $options);
+    $sql = "UPDATE Paper set timeFinalSubmitted=? WHERE paperId=?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([time(), $_POST['paperId']]);
+    $sql = "DELETE FROM PaperOption WHERE paperId=? AND optionId=?";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$_POST['paperId'], 3333]);
+    $sql = "INSERT INTO PaperOption (paperId,optionId,value) VALUES (?,?,1)";
+    $stmt = $db->prepare($sql);
+    $stmt->execute([$_POST['paperId'], 3333]);
     echo json_encode(array("response" => "ok"));
 } catch (PDOException $e) {
   showError('Database error: ' . $e->message());
